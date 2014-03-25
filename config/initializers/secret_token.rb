15080d3d5bc1bf67e73bee727a86cacbacf254f4
aulia <auliarahmahpimipindie@gmail.com>
2014-03-25 16:17:51 +0700

    juz_amma app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JuzAmma::Application.config.secret_token = '4d6478e13fdb783c096972737f552c38bf55c2a5dce13e7b5b2771a59285464b68086e78173ced502e0a3bb514109b3baff37457aff28810c7d290f4456e8dba'
